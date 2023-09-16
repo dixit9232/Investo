c4be7f1cec06d96295df82aaf2cc3f11517c2f71
Dixit Pambhar <141990205+dixit9232@users.noreply.github.com>
2023-09-16 17:54:08 +0530

    initial commit

--- lib/KYC/Step1.dart
+++ lib/KYC/Step1.dart
@@ -110,145 +110,157 @@ class _Step1State extends State<Step1> {
       backgroundColor: Theme.of(context).primaryColor,
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: IconButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            PageTransition(
-                                duration: Duration(milliseconds: 500),
-                                child: Login(),
-                                type: PageTransitionType.fade));
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        size: 30,
-                        color: Colors.white,
-                      )),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: IconButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                PageTransition(
+                                    duration: Duration(milliseconds: 500),
+                                    child: Login(),
+                                    type: PageTransitionType.fade));
+                          },
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 30,
+                            color: Colors.white,
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                        "Step 1",
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(fontSize: 25),
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  "Step 1",
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(fontSize: 25),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        "Take your photo",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayLarge!
+                            .copyWith(fontSize: 35),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    "Take your photo",
-                    style: Theme.of(context)
-                        .textTheme
-                        .displayLarge!
-                        .copyWith(fontSize: 35),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    """Make sure your face is not covered with any
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        """Make sure your face is not covered with any
 eyewear, jwellery or any other things.""",
-                    style: Theme.of(context).textTheme.titleSmall,
-                  ),
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SizedBox(
+                        height: 250,
+                        width: 250,
+                        child: (image_path == null)
+                            ? Image.asset(
+                                fit: BoxFit.fill, "assets/Images/camera 1.png")
+                            : Image.file(image_path!, fit: BoxFit.fill),
+                      ),
+                    ),
+                  ],
+                ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: SizedBox(
-                    height: 250,
-                    width: 250,
-                    child: (image_path == null)
-                        ? Image.asset(
-                            fit: BoxFit.fill, "assets/Images/camera 1.png")
-                        : Image.file(image_path!, fit: BoxFit.fill),
-                  ),
-                ),
               ],
             ),
-            SizedBox(height: 100,),
-            ElevatedButton(
-                style: ButtonStyle(
-                    fixedSize: MaterialStatePropertyAll(Size(350, 50)),
-                    shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                    backgroundColor:
-                        MaterialStatePropertyAll(Color(0xffA6A5EF))),
-                onPressed: () {
-                  get_permission_camera();
-                  getImage();
-                },
-                child: Text(
-                  "Take a Photo",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium!
-                      .copyWith(fontWeight: FontWeight.normal, fontSize: 25),
-                )),
-            SizedBox(height: 30),
-            OutlinedButton(
-                style: ButtonStyle(
-                    fixedSize: MaterialStatePropertyAll(Size(350, 50)),
-                    side: MaterialStatePropertyAll(BorderSide(
-                        style: BorderStyle.solid, color: Colors.white))),
-                onPressed: () {
-                  get_permission_gallary();
-                  choose_image();
-                },
-                child: Text(
-                  "Choose Photo",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium!
-                      .copyWith(fontWeight: FontWeight.normal, fontSize: 25),
-                )),
             Expanded(
-              child: Row(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.end,
+              child: Column(mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   ElevatedButton(
                       style: ButtonStyle(
                           fixedSize: MaterialStatePropertyAll(Size(350, 50)),
                           shape: MaterialStatePropertyAll(RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10))),
+                          backgroundColor:
+                          MaterialStatePropertyAll(Color(0xffA6A5EF))),
+                      onPressed: () {
+                        get_permission_camera();
+                        getImage();
+                      },
+                      child: Text(
+                        "Take a Photo",
+                        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            fontWeight: FontWeight.normal, fontSize: 25),
+                      )),
+                  SizedBox(height: 30),
+                  OutlinedButton(
+                      style: ButtonStyle(
+                          fixedSize: MaterialStatePropertyAll(Size(350, 50)),
+                          side: MaterialStatePropertyAll(BorderSide(
+                              style: BorderStyle.solid, color: Colors.white))),
+                      onPressed: () {
+                        get_permission_gallary();
+                        choose_image();
+                      },
+                      child: Text(
+                        "Choose Photo",
+                        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            fontWeight: FontWeight.normal, fontSize: 25),
+                      )),
+                  SizedBox(height: 30,),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          fixedSize: MaterialStatePropertyAll(Size(350, 50)),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10))),
                           backgroundColor: (image_path == null)
                               ? MaterialStatePropertyAll(Color(0x2ca6a5ef))
                               : MaterialStatePropertyAll(Color(0xffA6A5EF))),
                       onPressed: (image_path != null)
                           ? () {
-                              Navigator.pushReplacement(
-                                  context,
-                                  PageTransition(
-                                      duration: Duration(milliseconds: 500),
-                                      child: Confirm(image_path),
-                                      type: PageTransitionType.fade));
-                            }
+                        Navigator.pushReplacement(
+                            context,
+                            PageTransition(
+                                duration: Duration(milliseconds: 500),
+                                child: Confirm(image_path),
+                                type: PageTransitionType.fade));
+                      }
                           : null,
                       child: Text(
                         "Next",
-                        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(
                             fontWeight: FontWeight.normal,
-                            color:
-                                (image_path == null) ? Colors.white54 : Colors.white,
+                            color: (image_path == null)
+                                ? Colors.white54
+                                : Colors.white,
                             fontSize: 25),
                       )),
+                  SizedBox(height: 30,),
                 ],
               ),
-            ),
-            SizedBox(height: 30),
+            )
           ],
         ),
       ),
